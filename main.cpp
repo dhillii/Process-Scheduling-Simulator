b9ca9eb90692eca7def23cca36cf7e64fa94968d
David Hill <david.hill@bison.howard.edu>
2019-01-27 16:58:58 -0500

    Added RR Scheduler to Interface

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ char initPrompt();
 int main(){
     int numProc, quantum;
     int bt;
+    int burst_times[100];    
+    int processes[100];
     
 
     char choice = initPrompt();
@@ -16,13 +18,8 @@ int main(){
     switch(choice){
         case 'F':
         case 'f':
-           
             cout << " Enter the number of Processes: ";
             cin >> numProc;
-
-            int burst_times[100];
-            
-            int processes[100];
 
             for(int i = 0; i < numProc; i++){
                 int temp = i;   //protect i value from being mutated
@@ -33,12 +30,29 @@ int main(){
             }
 
             cout << endl << "....................FCFS SIMULATION...................." << endl;
-            FCFS_Scheduler Scheduler;
-            Scheduler.findavgTime(processes, numProc, burst_times);
+            FCFS_Scheduler F_Scheduler;
+            F_Scheduler.findavgTime(processes, numProc, burst_times);
             break;
 
         case 'R':
         case 'r':
+            cout << " Enter the number of Processes: ";
+            cin >> numProc;
+
+            cout << " Enter the Quantum: ";
+            cin >> quantum;
+
+            for(int i = 0; i < numProc; i++){
+                int temp = i;   //protect i value from being mutated
+                processes[temp] = temp+1;
+                cout << endl << " Enter the burst time for Process " << temp+1 <<": ";
+                cin >> bt;
+                burst_times[temp] = bt;
+            }
+
+            cout << endl << "....................ROUND ROBIN SIMULATION...................." << endl;
+            RR_Scheduler R_Scheduler;
+            R_Scheduler.findavgTime(processes, numProc, burst_times, quantum);
 
             break;
         case 'P':
@@ -49,15 +63,6 @@ int main(){
             cout << "The choice you entered is invalid. Exiting..." << endl;
             exit(0);
     }
-
-    /*RR_Scheduler Scheduler;
-
-    int processes[] = {1, 2, 3};
-    int n = sizeof processes / sizeof processes[0];
-    int  burst_time[] = {10, 5, 8}; 
-
-    Scheduler.findavgTime(processes, n, burst_time, 2);
-    */
     return(0);
 }
 
